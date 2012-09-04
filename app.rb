495822f838cdfc15d8379c7f0134db9418bb58f9
Gustavo C <p8queen@gmail.com>
2012-09-04 09:09:14 -0300

    admin sessions

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+enable :sessions
+
 get '/' do
   "Home page"
 end
@@ -8,10 +10,17 @@ get '/clima/:ciudad' do
   "Hello #{params[:ciudad]}"
 end
 
+get '/time/:ciudad' do
+   @t = Time.now
+   @city = params[:ciudad]	
+   haml :time
+  
+end
 
 post '/login' do
 	if params[:name] == 'admin' && params[:password] == 'admin'
 	  session['user_name'] = params[:name]
+	  redirect '/admin'
 	else
 	  redirect '/login'
 	end
@@ -27,10 +36,30 @@ before '/admin' do
     end
 end
 
+get '/admin' do
+  "Welcome Admin"
+end
 
 
 __END__
 
 @@ login
-%h1 login.
+%h1 login
+%form{ :action => "/login", :method => "post"}
+  %fieldset
+    %ol
+      %li
+        %label{:for => "name"} Name:
+        %input{:type => "text", :name => "name", :class => "text"}
+      %li
+        %label{:for => "password"} password:
+        %input{:type => "text", :name => "password", :class => "text"}
+      %input{:type => "submit", :value => "Login", :class => "button"}
+
+@@ time
+%p
+	Hora en #{@city}: #{@t.strftime("%T")}
+%p
+	Hora en UTC: #{@t.utc}
+
 
